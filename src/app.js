c6d5cee7d2a54248db99474da640b62956310f68
KmiloPena <125616941+KmiloPena@users.noreply.github.com>
2023-10-18 22:13:19 +0000

    New Changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  var x = document.getElementById("button");
+  x.addEventListener("click", generateRandomNumber);
+  x.addEventListener("click", generateRandomSuit);
 };
+
+function generateRandomNumber() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let randomNumb = Math.floor(Math.random() * numbers.length);
+  return numbers[randomNumb];
+}
+function generateRandomSuit() {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  return suit[randomSuit];
+}
